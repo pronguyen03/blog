714d92c3294bde37ae93f2c59f6d50c3f09452e1
Nhan NQ <nhannq@stageit.app>
2020-06-30 23:32:05 +0700

    - add plugins

--- scully.ngx-scully-blog.config.ts
+++ scully.ngx-scully-blog.config.ts
@@ -1,8 +1,24 @@
-import { ScullyConfig } from '@scullyio/scully';
+import { getSitemapPlugin } from '@gammastream/scully-plugin-sitemap';
+import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
+
+const { MinifyHtml } = require('scully-plugin-minify-html');
+const { DisableAngular } = require('scully-plugin-disable-angular');
+const postRenderers = [DisableAngular, MinifyHtml]
+
+const SitemapPlugin = getSitemapPlugin();
+setPluginConfig(SitemapPlugin, {
+  urlPrefix: 'https://nhannguyendacoder.web.app',
+  sitemapFilename: 'sitemap.xml',
+  changeFreq: 'monthly',
+  priority: ['1.0', '0.9', '0.8', '0.7', '0.6', '0.5', '0.4', '0.3', '0.2', '0.1', '0.0'],
+  ignoredRoutes: ['/404']
+});
+
 export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "ngx-scully-blog",
   outDir: './dist/static',
+  defaultPostRenderers: postRenderers,
   routes: {
     '/blog/:slug': {
       type: 'contentFolder',
