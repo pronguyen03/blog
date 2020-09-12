827cccbff06a067fdc2543e98c995e8866e0e106
Nhan NQ <nhannq@stageit.app>
2020-09-12 11:37:02 +0700

    - list categories

--- src/app/features/blog/blog-categories/blog-categories.component.ts
+++ src/app/features/blog/blog-categories/blog-categories.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CATEGORIES } from 'src/app/constants';
 
 @Component({
   selector: 'app-blog-categories',
   templateUrl: './blog-categories.component.html',
   styleUrls: ['./blog-categories.component.scss']
 })
-export class BlogCategoriesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class BlogCategoriesComponent {
+  CATEGORIES = CATEGORIES
 }
